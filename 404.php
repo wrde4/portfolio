a73fb8c4afc947b0088f62ddde0f794ed0192b6c
Jack Butts <jack@jackbutts.com>
2016-09-15 13:42:32 -0400

    Initial commit.

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?><br><br><br><br>
+<video preload="auto" autoplay="autoplay" loop="loop">
+    <source src="/images/oops.webm" type="video/webm"></source>
+</video>
+<p class="oops">Whoops!</p>
+<p>I can't find the page you're looking for. Retrace your steps, and if you still need help, please <a href="/contact">contact</a> me.</p>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,231 @@
+<!doctype html>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Jack Butts | Portfolio</title>
+
+		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,700italic%7CReem+Kufi%7CRoboto+Condensed:300,400,700%7CLibre+Baskerville:400,700%7CJosefin+Sans:700' rel='stylesheet'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/styles.min.css">
+	</head>
+	<body>
+		<?php include_once("analytics.php") ?>
+		<header>
+			<nav>
+				<div id="mobile-nav">
+					<img src="/images/jb-logo.png" width="50" alt="Home"/>
+					<a id="trigger" href="#"><i class="fa fa-bars hamburger-icon" aria-hidden="true"></i></a>
+				</div>
+				<ul>
+					<li id="home"><img src="/images/jb-logo.png" width="50" alt="Home"/></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#process">Process</a></li>
+					<li><a href="#work">Work</a></li>
+					<li><a href="#testimonials">Testimonials</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<section id="hello">
+				<p class="big-text">Hey, I'm Jack.</p>
+				<p class="intro">I'm a Georgia-based web developer and UX designer. My speciality is the user-centered design process: research, design, development, and evaluation. The sites I create are attractive, highly usable, and easy to maintain.</p>
+			</section>
+
+			<section id="about">
+				<div class="row">
+					<h2 class="decorated"><span>About Me</span></h2>
+					<div class="col-md-4 col-lg-3">
+						<img class="img-fluid" src="/images/headshot.png" alt="Picture of Jack">
+					</div>
+					<div class="col-md-8 col-lg-9">
+						<p>My name is Jack Butts, and I'm a freelance web developer and UX designer. I recently graduated with an M.A. from Clemson University, and I'm now seeking employment. My coursework and self-taught expertise have trained me to prototype beautiful, usable designs and also code them at a production level.</p>
+						<p>Interested in my work? I'd love to <a href="#contact">get in touch</a>.</p>
+						<button class="btn"><a target="_blank" href="/files/jb-resume.pdf"><i class="fa fa-download"></i><span>Download my resume</span></a></button>
+					</div>
+				</div>
+			</section>
+
+			<section id="process">
+				<h2 class="decorated"><span>My Process</span></h2>
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="media">
+							<div class="media-left">
+								<i class="fa fa-fw fa-users" aria-hidden="true"></i>
+							</div>
+							<div class="media-body">
+								<h3>1. Research</h3>
+								<p>Any user-centered design project begins with learning about users. I perform task analyses and interview authentic users&mdash;the real people who will be using your product&mdash;to discover their needs. Depending on the size of the product's audience, surveying can also be a meaningful way of capturing user input.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="media">
+							<div class="media-left">
+								<i class="fa fa-fw fa-eye" aria-hidden="true"></i>
+							</div>
+							<div class="media-body">
+								<h3>2. Design</h3>
+								<p>Using the collected research, I design <a href="/images/work/persona1.png" data-rel="lightcase:personas" title="Persona #1 for Margins Digital Humanities Conference">personas</a> to help me keep track of the target user groups. Then, Photoshop and Axure are used to fashion <a href="/images/work/wireframe1.png" data-rel="lightcase:wireframes" title="Wireframe #1: Homepage for The Happy Berry">wireframes</a> and <a href="/images/work/prototype1.png" data-rel="lightcase:prototypes" title="Prototype #1: Homepage for The Happy Berry">prototypes</a>. These early visual representations for the project allow me to share my progress and solicit feedback before development begins.</p>
+								<a href="/images/work/persona2.png" data-rel="lightcase:personas" title="Persona #2 for Margins Digital Humanities Conference"></a>
+								<a href="/images/work/persona3.png" data-rel="lightcase:personas" title="Persona #3 for Margins Digital Humanities Conference"></a>
+								<a href="/images/work/wireframe2.png" data-rel="lightcase:wireframes" title="Wireframe #2: Content Page for The Happy Berry"></a>
+								<a href="/images/work/wireframe3.png" data-rel="lightcase:wireframes" title="Wireframe #3: Mobile Homepage for The Happy Berry"></a>
+								<a href="/images/work/prototype2.png" data-rel="lightcase:prototypes" title="Prototype #2: Content Page for The Happy Berry"></a>
+								<a href="/images/work/prototype3.png" data-rel="lightcase:prototypes" title="Prototype #3: Mobile Homepage for The Happy Berry"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="media">
+							<div class="media-left">
+								<i class="fa fa-fw fa-pencil" aria-hidden="true"></i>
+							</div>
+							<div class="media-body">
+								<h3>3. Development</h3>
+								<p>Moving from concept to code, my development process consists of building websites using HTML 5, CSS 3, JavaScript, and PHP. Utilizing jQuery and SASS makes life easier, and I often turn to either Bootstrap or Foundation when I need a responsive framework.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="media">
+							<div class="media-left">
+							<i class="fa fa-fw fa-check-square-o" aria-hidden="true"></i>
+							</div>
+							<div class="media-body">
+								<h3>4. Evaluation</h3>
+								<p>A project can't be deemed successful until real users have evaluated it. After performing a heuristic evaluation of the website myself, I conduct usability tests. During these tests, I ask participants to complete tasks on the website. This informal testing helps uncover functional problems with the website that are fixed before the final version is delivered to the client.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+<section id="work">
+	<h2 class="decorated"><span>Recent Work</span></h2>
+	<div class="row col-centered projects-list">
+		<div class="project-block">
+		<a target="_blank" href="http://hb.jackbutts.com">
+			<img class="img-fluid" src="/images/work/thb-screenshot.png" alt="">
+			<p>The Happy Berry</p>
+		</a>
+		</div>
+				
+		<div class="project-block">
+		<a target="_blank" href="http://margins.jackbutts.com">
+			<img class="img-fluid" src="/images/work/margins-screenshot.png" alt="">
+			<p>Margins Digital Humanities Conference</p>
+		</a>
+		</div>
+			
+		<div class="project-block">
+		<a target="_blank" href="http://www.clemson.edu/centers-institutes/cva">
+			<img class="img-fluid" src="/images/work/cva-screenshot.png" alt="">
+			<p>Clemson Center for Visual Arts</p>
+		</a>
+		</div>	
+			
+		<div class="project-block">
+		<a target="_blank" href="http://jackbutts.com/play">
+			<img class="img-fluid" src="/images/work/play-screenshot.png" alt="">
+			<p>Composition Pedagogy Webtext</p>
+		</a>
+		</div>
+	</div>
+</section>
+
+<section id="testimonials">
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="decorated"><span>Testimonials</span></h2>
+			<div class="flexslider">
+				<ul class="slides">
+					<li>
+						<q>Jack asked smart questions, and always made sure he understood not only the technical aspects but also the 'vision' we were after. He inspired confidence in us and what we were doing, and turned what could have been a stressful process into something enjoyable and stimulating.</q>
+						<p>&mdash; Jason Crider, University of Florida</p> 
+					</li>
+					<li>
+						<q>He’s so easy to get along with and made the projects we worked on a pleasant experience. He does his part in getting the work done and more often than not does more than is expected of him. Jack would be a strong asset to any team he is on and is GREAT for company culture!</q>
+						<p>&mdash; Mary Dickens, Technical Writer at Cengage Learning</p>
+					</li>
+					<li>
+						<q>I would absolutely recommend him. He is so nice and helpful and never tears you down for your ideas or opinions. He is very prepared for class every day, he is never late and he always has interesting material to go over. He expects you to do your work, and if you do you will get a lot out of the class.</q>
+						<p>&mdash; Anonymous Student Evaluation</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="contact">
+	<div id="contact-methods">
+		<h2 class="decorated"><span>Let's Chat</span></h2>
+		<div class="contact-instructions">Email, phone, or find me online&hellip;</div>
+		<div id="contact-block-container">	
+			<div class="contact-block">
+				<i class="fa fa-envelope" aria-hidden="true"></i>
+				<p>jack@<span class="display-none">null</span>jackbutts.com</p>
+			</div>
+		
+				
+			<div class="contact-block">
+				<i class="fa fa-phone" aria-hidden="true"></i>
+				<p><a href="tel:8642145531">(864) 214-5531</a></p>
+			</div>
+				
+				
+			<div class="contact-block">
+				<a href="http://linkedin.com/in/jackbutts" target="_blank">
+					<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+					<p>LinkedIn</p>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div id="contact-form">
+		<div class="contact-instructions">
+			&hellip;or use this handy form!
+		</div>
+		<form role="form" id="ajax-contact" method="post" action="/includes/mailer.php">
+			<div class="row">
+				<div id="form-messages"></div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 offset-lg-2 col-md-4 offset-md-1">
+					<label for="name">Name
+						<input type="text" name="name" id="name" required>
+					</label>
+
+					<label for="email">Email Address
+						<input type="email" name="email" id="email" required>
+					</label>
+				</div>
+				<div class="col-lg-4 offset-lg-1 col-md-5 offset-md-1">
+					<label for="message">Message
+						<textarea name="message" id="message"></textarea>
+					</label>
+				</div>
+			</div>
+					
+			<div id="submit-container" class="row">
+				<div class="col-lg-10 offset-lg-2 offset-md-1">
+					<input type="submit" name="submit" class="hvr-wobble-bottom" id="submit" value="Submit">
+				</div>
+			</div>
+		</form>
+	</div>
+</section>
+</main>
+<footer>
+	&copy; Jack Butts, <?php date_default_timezone_set('America/New_York'); echo date("Y"); ?>. All rights reserved.
+	<div id="to-top"><a class="scroll-top"><i class="fa fa-arrow-up hvr-grow" aria-hidden="true"></i></a></div>
+</footer>
+	<script src="js/main.min.js"></script>
+</body>
+</html>
